9c3cbc7f9a3ee055ca09d82011ce1a89c0ca91dd
sulthanmuh51 <sulthanmuh51@gmail.com>
2025-07-23 04:54:43 +0700

    fix js issues

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
-  $(".card").on("click", function () {
+  $(document).on("click", ".card", function () {
     window.location.href = "./work.html";
+  });
+
+  $("#back-btn").on("click", function () {
+    console.log(1);
+    history.back();
   });
 });
